129ac44cd13906b3e20e1deae66d8804b56f53f5
Marcin <marcin.sulecki@altkom.pl>
2019-04-26 02:59:27 +0200

    Nawigacja z ViewModel

--- Altkom.ABC.ViewModels/RelayCommand.cs
+++ Altkom.ABC.ViewModels/RelayCommand.cs
@@ -5,6 +5,29 @@ using System.Windows.Input;
 
 namespace Altkom.ABC.ViewModels
 {
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;
+        private readonly Func<T, bool> canExecute;
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => canExecute == null ? true : canExecute.Invoke((T) parameter);
+
+        public void Execute(object parameter) => execute?.Invoke((T) parameter);
+
+        public void OnCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
     public class RelayCommand : ICommand
     {
         private readonly Action execute;
--- Altkom.ABC.ViewModels/ShellViewModel.cs
+++ Altkom.ABC.ViewModels/ShellViewModel.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using Altkom.ABC.FakeServices;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Altkom.ABC.ViewModels
 {
     public class ShellViewModel : ViewModelBase
     {
+        private ViewModelBase _selectedViewModel;
+
         public string Name { get; set; }
+
+        public ICommand ShowViewCommand { get; private set; }
+
+        public ViewModelBase SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         public ShellViewModel()
         {
             Name = "Hello";
+
+            ShowViewCommand = new RelayCommand<string>(p => ShowView(p));
+
+            SelectedViewModel = new CustomersViewModel(new FakeCustomersService(new FakeServices.Fakers.CustomerFaker()));
+        }
+
+        private void ShowView(string view)
+        {
+            SelectedViewModel = new ProductsViewModel(new FakeProductsService(new FakeServices.Fakers.ProductFaker()));
         }
     }
 }
