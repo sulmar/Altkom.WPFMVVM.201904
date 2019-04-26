e3e992008280b60fbeb8a4a0c10bafbc9122651b
Marcin <marcin.sulecki@altkom.pl>
2019-04-26 06:57:06 +0200

    EventAggregator

---
+++ Altkom.ABC.WPFClient/Common/EventAggreator.cs
@@ -0,0 +1,53 @@
+﻿using Altkom.ABC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.ABC.WPFClient.Common
+{
+    
+
+    public interface IEvent
+    {
+
+    }
+
+    public interface IEventAggregator
+    {
+        void Register<T>(Action<T> action)
+                where T : IEvent;
+        void Send<T>(T message);
+
+    }
+
+    public class EventAggreator : IEventAggregator
+    {
+        private readonly Dictionary<Type, List<Action<IEvent>>> allObservers;
+
+        public EventAggreator()
+        {
+            allObservers = new Dictionary<Type, List<Action<IEvent>>>();
+        }
+
+        public void Register<T>(Action<T> action)
+            where T : IEvent
+        {
+            //  List<Action<T>> observers;
+
+            List<Action<IEvent>> observers = new List<Action<IEvent>>();
+
+            allObservers.TryGetValue(typeof(T), out observers);
+            
+            // observers.Add(action);
+
+            // observers.Add(action);
+        }
+
+        public void Send<T>(T message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
